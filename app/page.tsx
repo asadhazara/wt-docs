041d37bace8e029b420d03edfb504f98629b351d
Jurian Vink <>
2025-02-25 10:58:24 +0100

    add code for doc notion

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,240 @@
-import Image from "next/image";
+import cn from 'classnames';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'AI Docs — wetracked.io',
+  description: 'AI Docs',
+};
+
+type IText = {
+  type: string;
+  text: {
+    content: string;
+    link: null;
+  };
+  annotations: {
+    bold: boolean;
+    italic: boolean;
+    strikethrough: boolean;
+    underline: boolean;
+    code: boolean;
+    color: 'default';
+  };
+  plain_text: string;
+  href: null;
+};
+
+type Content = {
+  rich_text: IText[];
+  color: string;
+};
+
+type ParagraphBlock = {
+  type: 'paragraph';
+  paragraph: Content;
+};
+
+type Heading1Block = {
+  type: 'heading_1';
+  heading_1: Content;
+};
+
+type Heading2Block = {
+  type: 'heading_2';
+  heading_2: Content;
+};
+
+type Heading3Block = {
+  type: 'heading_3';
+  heading_3: Content;
+};
+
+type CalloutBlock = {
+  type: 'callout';
+  callout: Content;
+};
+
+type NumberedListBlock = {
+  type: 'numbered_list_item';
+  numbered_list_item: Content;
+};
+
+type BulletedListBlock = {
+  type: 'bulleted_list_item';
+  bulleted_list_item: Content;
+};
+
+type CodeBlock = {
+  type: 'code';
+  code: Content;
+};
+
+type Block = {
+  id: string;
+} & (
+  | ParagraphBlock
+  | Heading1Block
+  | Heading2Block
+  | Heading3Block
+  | CalloutBlock
+  | NumberedListBlock
+  | BulletedListBlock
+  | CodeBlock
+);
+
+type NotionPage = {
+  object: string;
+  results: Block[];
+  next_cursor: null;
+  has_more: boolean;
+  type: 'block';
+  block: unknown;
+  request_id: string;
+};
+
+const fetchNotionPage = async (PAGE_ID: string): Promise<NotionPage> => {
+  const result = await fetch(`https://api.notion.com/v1/blocks/${PAGE_ID}/children`, {
+    cache: 'no-store',
+    headers: {
+      Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28',
+    },
+  });
+  return await result.json();
+};
+
+const getNotionPages = async () => {
+  const PAGE_IDs = [
+    '6f09a80ccaa54bcb87fbbd22c3771de1', // Common problems
+    'fd29cb44c260407bb1fc85bb818e5562', // Integrations main page
+    'cf70e76a367b48f98e6e244d5230b4cf', // Shopify
+    '4d03fa1671594f2197172dd0936a713d', // Funnelish
+    '2c28311a76864bd3b30cae1e4108a938', // External checkouts
+    'f50042e583e942b6834a3236b85769fe', // Meta Ads
+    '097b9181305046ad8082f14211e4078a', // Google Ads
+    'c6cbf58297a24575a39f5ac08e2fc412', // Pinterest Ads
+    'cbab069422d34bf6b67dda3b7a5adc15', // X Ads
+    'da202a7f20b64d42904662c39e2ab13c', // TikTok Ads
+    '03645f24daf44becb126e3a52ba0b921', // Snapchat Ads
+    '4d78b88568c34c20b9f0eb8d1dd2c488', // Klaviyo
+    '872c79a3d6e54273aecef221755f1d07', // Advertorials
+  ];
+  return await Promise.all(PAGE_IDs.map(id => fetchNotionPage(id)));
+};
+
+const Text = ({ text }: { text: IText }) => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <span
+      className={cn(
+        text.annotations.bold && 'font-bold',
+        text.annotations.italic && 'italic',
+        text.annotations.strikethrough && 'line-through',
+        text.annotations.underline && 'underline',
+        text.annotations.code && 'bg-gray-100 p-1 rounded-md'
+      )}
+    >
+      {text.text.content}
+    </span>
   );
-}
+};
+
+const NotionPage = ({ page }: { page: { results: Block[] } }) => {
+  if (!page.results) return null;
+  return (
+    <article>
+      {page.results.map((block: Block) => {
+        if (block.type === 'heading_1') {
+          return (
+            <h1 key={block.id}>
+              {block.heading_1.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </h1>
+          );
+        }
+        if (block.type === 'heading_2') {
+          return (
+            <h2 key={block.id}>
+              {block.heading_2.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </h2>
+          );
+        }
+        if (block.type === 'heading_3') {
+          return (
+            <h3 key={block.id}>
+              {block.heading_3.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </h3>
+          );
+        }
+        if (block.type === 'paragraph') {
+          return (
+            <p key={block.id}>
+              {block.paragraph.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </p>
+          );
+        }
+        if (block.type === 'callout') {
+          return (
+            <div key={block.id} className="bg-blue-100 p-4 rounded-md">
+              {block.callout.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </div>
+          );
+        }
+        if (block.type === 'numbered_list_item') {
+          return (
+            <li key={block.id}>
+              {block.numbered_list_item.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </li>
+          );
+        }
+        if (block.type === 'bulleted_list_item') {
+          return (
+            <li key={block.id}>
+              {block.bulleted_list_item.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </li>
+          );
+        }
+        if (block.type === 'code') {
+          return (
+            <pre key={block.id}>
+              {block.code.rich_text.map((t: IText, index: number) => (
+                <Text key={index} text={t} />
+              ))}
+            </pre>
+          );
+        }
+        return null;
+      })}
+    </article>
+  );
+};
+
+const Page = async ({ searchParams: { token } }: { searchParams: { token: string } }) => {
+  if (token !== process.env.ASSISTANT_TOKEN) return notFound();
+
+  const pages = await getNotionPages();
+
+  return (
+    <>
+      {pages.map(page => (
+        <NotionPage key={page.request_id} page={page} />
+      ))}
+    </>
+  );
+};
+
+export default Page;
